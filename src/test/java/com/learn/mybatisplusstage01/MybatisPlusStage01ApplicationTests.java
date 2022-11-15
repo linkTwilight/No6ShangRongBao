3e3d64113f1a5eebb6a3c8f393b7ba169f72fb0e
ljy <link.twilight.gu@gmail.com>
2022-11-15 19:05:47 +0800

    复杂查询

--- src/test/java/com/learn/mybatisplusstage01/MybatisPlusStage01ApplicationTests.java
+++ src/test/java/com/learn/mybatisplusstage01/MybatisPlusStage01ApplicationTests.java
@@ -1,5 +1,7 @@
 package com.learn.mybatisplusstage01;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.learn.mybatisplusstage01.entity.User;
 import com.learn.mybatisplusstage01.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
@@ -16,8 +18,45 @@ class MybatisPlusStage01ApplicationTests {
 
     @Test
     void contextLoads() {
+
+    }
+
+    @Test
+    public void testSelectAll() {
+        // 查询所有
         List<User> users = userMapper.selectList(null);
         users.forEach(System.out::println);
     }
+
+    @Test
+    public void testSelectByConditions() {
+        // 条件查询
+/*        User user = userMapper.selectById(1);
+        System.out.println(user);*/
+//        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+/*        queryWrapper.eq("name","Sandy");
+        System.out.println(userMapper.selectOne(queryWrapper));*/
+/*        queryWrapper.gt("age",20);
+        queryWrapper.lt("age",25);
+        System.out.println(userMapper.selectList(queryWrapper));*/
+// 使用lambda
+/*        System.out.println(userMapper.selectList(new LambdaQueryWrapper<User>()
+                .gt(User::getAge, 20)
+                .lt(User::getAge, 25)
+                .like(User::getName, "a")
+                .orderByDesc(User::getAge)
+                .select(User::getName, User::getAge)
+        ));*/
+
+        System.out.println(userMapper.selectList(new LambdaQueryWrapper<User>()
+                .gt(User::getAge, 18)
+                .eq(User::getAge, 28)
+                .or()
+                .like(User::getName, "o")
+        ));
+    }
+
+    // 新增
+
 
 }
